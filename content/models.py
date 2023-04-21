a8e645771d36ba968f9a73c2858d9774ee272f98
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-21 16:02:26 +0200

    Added migrations

--- content/models.py
+++ content/models.py
@@ -22,7 +22,7 @@ class Image(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
-    content = models.CharField(500)
+    content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
