aa19e36db17599e64b50b917de30dafe4ed5d03b
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-21 16:00:09 +0200

    Registered modelis in admin.py

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from content.models import Post, Image, Comment, Like
+
+admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
