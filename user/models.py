ceed5669952595a27cc1ee54e905e687738710f6
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-21 15:56:10 +0200

    Added UserFollowing model to user app

--- user/models.py
+++ user/models.py
@@ -1,5 +1,35 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from config.settings import AUTH_USER_MODEL
 
 
 class User(AbstractUser):
     pass
+
+    def __str__(self) -> str:
+        return (
+            f"{self.username} "
+            f"({self.first_name}"
+            f" {self.last_name})"
+        )
+
+
+class UserFollowing(models.Model):
+    following = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="following",
+        on_delete=models.CASCADE
+    )
+    followers = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="followers",
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return (
+            f"Followers: {self.followers.username}"
+            f"Following: {self.followers.username}"
+        )
