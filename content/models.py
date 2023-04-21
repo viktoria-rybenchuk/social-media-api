ee094034195a59fa380653e62526b66d5abab8e0
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-21 15:24:01 +0200

    Added Image, Comment, and Like models to content app

--- content/models.py
+++ content/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from config.settings import AUTH_USER_MODEL
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="post_images", null=True, blank=True)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes")
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
