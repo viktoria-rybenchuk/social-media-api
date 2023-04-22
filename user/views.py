8beaa05f2f7b216f0fb918d89be52b2ca44bb412
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-22 20:28:46 +0200

    Added view for User model & added action for updation or creating profile image

--- user/views.py
+++ user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from user.serializers import UserListSerializer, UserDetailSerializer, UserSerializer, ProfileImageSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserDetailSerializer
+        if self.action == "list":
+            return UserListSerializer
+        if self.action == "upload_image":
+            return ProfileImageSerializer
+        return self.serializer_class
+
+    @action(
+        methods=["POST","GET"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific profile"""
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
