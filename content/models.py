c61886933063f0935736b105fa211792b7cafd0b
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-23 21:18:02 +0200

    Added model Tag & added migrations

--- content/models.py
+++ content/models.py
@@ -4,7 +4,11 @@ from config.settings import AUTH_USER_MODEL
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to="post_images", null=True, blank=True)
+    image = models.ImageField(
+        upload_to="post_images",
+        null=True,
+        blank=True
+    )
 
 
 class Post(models.Model):
@@ -12,24 +16,65 @@ class Post(models.Model):
     content = models.CharField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
-    images = models.ManyToManyField(Image, null=True, related_name="posts", blank=True)
+    author = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    images = models.ManyToManyField(
+        Image,
+        related_name="posts",
+        blank=True
+    )
+
+    class Meta:
+        ordering = ("created_at",)
 
     def __str__(self) -> str:
         return self.title
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ("created_at",)
+
+    def __str__(self) -> str:
+        return self.content
+
 
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+
+    class Meta:
+        unique_together = ("post", "user")
 
     def __str__(self) -> str:
         return f"{self.user.username} liked {self.post.title}"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=60)
+    posts = models.ManyToManyField(Post, related_name="tags")
